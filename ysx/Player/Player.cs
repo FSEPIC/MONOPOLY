5140a610719c9b1a0d3e7997d9162f0cdc2649e2
ysx1024 <2650974406@qq.com>
2020-05-28 08:49:19 +0800

    对Player类和Event类的修改

--- ysx/Player/Player.cs
+++ ysx/Player/Player.cs
@@ -9,6 +9,7 @@ namespace Player
         public int Location { set; get; }//位置
         public int Pause { set; get; }   //暂停回合数
         public int Money { set; get; }   //金钱
+        public int Turn { set; get; }   //剩余回合数
         public int Health { set; get; }  //健康值，最大值为100
         public int Grade { set; get; }   //成绩，最大值为750
     }
